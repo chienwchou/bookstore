d497d3bc9e8e628269e85cf26bf57298b08ea6e1
Chien Wei Chou <chien.w.chou@gmail.com>
2024-04-15 10:23:05 -0700

    refactor book model

--- app/models/book.ts
+++ app/models/book.ts
@@ -1,14 +1,11 @@
-export interface BookData {
-    _id: string;
-    title: string;
-    price: number;
-    category: string;
-    description: string;
-}
 
 export interface BookFormData {
     title: string;
     price: number;
     category: string;
     description: string;
+}
+
+export interface BookData extends BookFormData {
+    _id: string;
 }
