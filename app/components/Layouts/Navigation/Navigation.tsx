53d7e0fe68efc9c33aad96712b8005a16903d776
Chien Wei Chou <chien.w.chou@gmail.com>
2024-04-18 18:50:07 -0700

    add about page and navigation

--- app/components/Layouts/Navigation/Navigation.tsx
+++ app/components/Layouts/Navigation/Navigation.tsx
@@ -43,7 +43,7 @@ export function Navigation() {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
             <li>
               <a
-                href="#"
+                href="/"
                 className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                 aria-current="page"
               >
@@ -52,7 +52,7 @@ export function Navigation() {
             </li>
             <li>
               <a
-                href="#"
+                href="/about"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
@@ -63,7 +63,7 @@ export function Navigation() {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Services
+                Categories
               </a>
             </li>
             <li>
@@ -71,7 +71,7 @@ export function Navigation() {
                 href="#"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Pricing
+                Membership
               </a>
             </li>
             <li>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 import { useAppSelector } from "@/lib/store";
 import { BookData } from "./models/book";
 import { BookCard } from "./components/BookCard/BookCard";
-import { Navigation } from "./components/Layouts/Navigation/Navigation";
 
 export default function Home() {
   const booksData = useAppSelector((state) => {
@@ -11,7 +10,6 @@ export default function Home() {
 
   return (
     <>
-      <Navigation />
       <main className="flex min-h-screen flex-col items-center justify-between p-24 p-10">
         <ul className="grid grid-cols-3 gap-4">
           {booksData.map((book: BookData) => (
