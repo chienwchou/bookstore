f3e08f928e7912d05a6078f0f3df0546ea74937b
Chien Wei Chou <chien.w.chou@gmail.com>
2024-04-18 17:27:03 -0700

    add navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
-import Link from "next/link";
 import { useAppSelector } from "@/lib/store";
 import { BookData } from "./models/book";
 import { BookCard } from "./components/BookCard/BookCard";
+import { Navigation } from "./components/Layouts/Navigation/Navigation";
 
 export default function Home() {
   const booksData = useAppSelector((state) => {
@@ -11,18 +11,7 @@ export default function Home() {
 
   return (
     <>
-      <main>
-        <div className="m-5">
-          <h1 className="text-5xl font-bold text-center">Bookstore</h1>
-          <nav className="flex justify-between items-center">
-            <Link href="?modal=true">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                New book
-              </button>
-            </Link>
-          </nav>
-        </div>
-      </main>
+      <Navigation />
       <main className="flex min-h-screen flex-col items-center justify-between p-24 p-10">
         <ul className="grid grid-cols-3 gap-4">
           {booksData.map((book: BookData) => (
